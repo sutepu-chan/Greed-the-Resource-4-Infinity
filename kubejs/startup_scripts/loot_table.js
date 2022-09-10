538c5d55487d610f8a6e71d630a44be468da443d
sutepuchan <leewaikin0425@gmail.com>
2022-09-11 02:42:15 +0800

    V5.0
    Fix loot table for sawmill for unification

--- kubejs/startup_scripts/loot_table.js
+++ kubejs/startup_scripts/loot_table.js
@@ -1,3 +1,4 @@
 onEvent('block.loot_tables', e => {
     e.addSimpleBlock('charm:woodcutter', 'corail_woodcutter:oak_woodcutter')
+    e.addSimpleBlock('environmental:sawmill', 'corail_woodcutter:oak_woodcutter')
 })
