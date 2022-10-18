5c6dc7a4208f3a9fed0768025777f21a9af21756
sutepuchan <leewaikin0425@gmail.com>
2022-10-19 03:15:44 +0800

    V6.0
    Removing old Avaritia items
    Adding set effects of new Avaritia items

--- kubejs/server_scripts/avaritia_armor.js
+++ kubejs/server_scripts/avaritia_armor.js
@@ -2,19 +2,19 @@ onEvent("player.tick", e => {
     const { headArmorItem, chestArmorItem, legsArmorItem, feetArmorItem } = e.player;
     //e.player.potionEffects.add(effect, duration(ticks), level, durability)
     if (e.player.level.time%100 == 0) {
-        if (headArmorItem.id === 'upgradednetherite_creative:creative_upgraded_netherite_helmet') {
+        if (headArmorItem.id === 'endless:infinity_helmet') {
             e.player.potionEffects.add('night_vision', 400, 0)
         }
-        if (chestArmorItem.id === 'upgradednetherite_creative:creative_upgraded_netherite_chestplate') {
+        if (chestArmorItem.id === 'endless:infinity_chestplate') {
             e.player.potionEffects.add('absorption', 400, 9)
         }
-        if (legsArmorItem.id === 'upgradednetherite_creative:creative_upgraded_netherite_leggings') {
+        if (legsArmorItem.id === 'endless:infinity_leggings') {
             e.player.potionEffects.add('jump_boost', 400, 0)
         }
-        if (feetArmorItem.id === 'upgradednetherite_creative:creative_upgraded_netherite_boots') {
+        if (feetArmorItem.id === 'endless:infinity_boots') {
             e.player.potionEffects.add('speed', 400, 1)
         }
-        if (headArmorItem.id === 'upgradednetherite_creative:creative_upgraded_netherite_helmet' && chestArmorItem.id === 'upgradednetherite_creative:creative_upgraded_netherite_chestplate' && legsArmorItem.id === 'upgradednetherite_creative:creative_upgraded_netherite_leggings' && feetArmorItem.id === 'upgradednetherite_creative:creative_upgraded_netherite_boots') {
+        if (headArmorItem.id === 'endless:infinity_helmet' && chestArmorItem.id === 'endless:infinity_chestplate' && legsArmorItem.id === 'endless:infinity_leggings' && feetArmorItem.id === 'endless:infinity_boots') {
             e.player.potionEffects.add('strength', 400, 9)
             e.player.potionEffects.add('luck', 400, 4)
         }
