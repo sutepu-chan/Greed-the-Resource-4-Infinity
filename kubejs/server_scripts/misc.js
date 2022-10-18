c8fb4df4e2efce6f9750298ef68d1ffb543924ea
sutepuchan <leewaikin0425@gmail.com>
2022-10-19 03:17:50 +0800

    V6.0
    Modify recipes of better muffling

--- kubejs/server_scripts/misc.js
+++ kubejs/server_scripts/misc.js
@@ -219,4 +219,18 @@ onEvent('recipes', e => {
         G: '#forge:gears/iron',
         S: '#forge:rods/wooden'
     })
+    
+    //===== better muffling =====//
+    modifyShaped(e, 'bettermuffling:muffling_block', 1, ['MWM', 'WNW', 'MWM'], {
+        W: '#forge:wool',
+        N: 'minecraft:note_block',
+        M: 'mekanism:upgrade_muffling'
+    })
+    modifyShaped(e, 'bettermuffling:upgrade', 1, ['GEG', 'EME', 'GEG'], {
+        M: 'bettermuffling:muffling_block',
+        G: 'allthecompressed:gold_block_1x',
+        E: '#forge:gears/gold'
+    })
+    e.remove({id: 'bettermuffling:muffling_block_advanced'})
+    e.shapeless('bettermuffling:muffling_block_advanced', ['bettermuffling:muffling_block', 'bettermuffling:upgrade']).id(`kubejs:bettermuffling/muffling_block_advanced`)
 })
